5fdfae3bc2518114661328ccf2e66062844e5281
Shannon <shannon@mada.us>
2014-08-15 15:29:05 -0400

    Submission Coopy
    Original code commented, modified and tested.  Appears to work as per
    assignment specification.

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code creates a special matrix and caches the inverse
+## Objective: If matrix doesn't change, and inverse exists in cache, retrieve it
 
-## Programming Assignment 2
-## Shannon Mada
-## Friday, August 15th, 2014
+# Function 1: makeCacheMatrix 
+# Argument required is a square matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Function 2: cacheSolve
+## If inverse exists in cache and matrix has not changed, use it
+## Otherwise, calculate for the first time drawing from first function def.'s
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+       
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
+}        
